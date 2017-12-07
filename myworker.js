86328ff75128f43268ef0f5e90b416e04876c104
Vicent Pérez <vicennt@gmail.com>
2017-12-07 18:23:19 +0100

    Basic zmq worker implementation

---
+++ myworker.js
@@ -0,0 +1,20 @@
+var zmq = require("zqm");
+var resp = zmq.socket("req");
+var args = process.argv.slice(2);
+var backend_url = args[0];
+var my_id = args[1];
+var con_text = "id";
+var resp_text = args[3];
+
+resp.indentity = my_id;
+resp.connect(backend_url);
+resp.on("message", function(client, delimiter, msg){
+	setTimeout(function(){
+		resp.send([client,'',resp_text]);
+	}, 1000);
+});
+
+resp.send(con_text);
+
+
+
