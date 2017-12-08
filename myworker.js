42189e72ebdaf1a902c134e30c87abe7d3617a83
Vicent Pérez <vicennt@gmail.com>
2017-12-08 12:26:41 +0100

    Verbose mode in workers

--- myworker.js
+++ myworker.js
@@ -5,16 +5,38 @@ var backend_url = args[0];
 var my_id = args[1];
 var con_text = "id";
 var resp_text = args[2];
+var verbose = false;
+
+if(args[args.length - 1] == "-v"){
+    verbose = true;
+    args.pop();
+}
 
 resp.indentity = my_id;
 resp.connect(backend_url);
+
+if(verbose){
+    console.log('Worker (%s) connected to "%s"', my_id, backend_url);
+}
+
 resp.on("message", function(client, delimiter, msg){
+    i(verbose){
+        console.log('Worker (%s) has received request "%s" from client (%s)',
+            my_id, msg.toString(), client);
+    }
 	setTimeout(function(){
 		resp.send([client,'',resp_text]);
+		if(verbose){
+		    console.log('Worker (%s) has sent its reply "%s"', my_id, resp_text);
+        }
 	}, 1000);
 });
 
 resp.send(con_text);
+
+if(verbose){
+    console.log('Worker (%s) has sent its first connection message: "%s"', my_id, con_text);
+}
 
 
 
