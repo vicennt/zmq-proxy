2f918969bda02416deeef1fa74c548e0901869d1
Vicent Pérez <vicennt@gmail.com>
2017-12-09 17:05:40 +0100

    Add type arg in client and send this type in msg

--- myclient.js
+++ myclient.js
@@ -8,7 +8,9 @@ var args = process.argv.slice(2);
 var frontend_url = args[0] || "tcp://localhost:8059";
 var my_id = args[1] || "NONE";
 var my_msg = args[2] || "Work";
+var class_id = args[3] || "B";
 var verbose = false;
+
 
 if(args[args.length - 1] == "-v"){
 	verbose = true;
@@ -19,10 +21,9 @@ if(my_id != "NONE")
     req.identity = my_id;
 
 req.connect(frontend_url);
-req.send(my_msg); // Ex. ["","Work"] (socket add delimiter)
+req.send([my_msg, class_id]); // Ex. ["","Work","B"] (socket add delimiter)
 if(verbose)
-	console.log('Client (%s) connected to "%s" send this msg -> (%s)', my_id, frontend_url, my_msg);
-
+    console.log('Client (%s), class (%s) connected to %s', my_id, class_id, frontend_url);
 req.on("message", function(msg){
     if(verbose)
 	    console.log('Client (%s) has received reply "%s"', my_id, msg.toString());
