cd63d5f27688fd055f19a3766e8ed04dc86b01f1
Vicent Pérez <vicennt@gmail.com>
2017-12-09 18:56:43 +0100

    Implement heardbeat

--- mybroker.js
+++ mybroker.js
@@ -1,4 +1,4 @@
-// Execution example -> $node mybroker.js 8059 8060 -v
+// Execution example -> $node mybroker.js 8059 8060 A B C D-v
 
 var zmq = require('zmq');
 var aux = require('./auxfunctions')
@@ -8,21 +8,26 @@ var backend = zmq.socket('router');
 var args = process.argv.slice(2);
 var frontend_port = args[0] || 8059;
 var backend_port = args[1] || 8060;
-
+var args = args.slice(2); // Remove ports
 // Array of available workers
 var workers = [];
 // Array of pending clients
 var clients = [];
 // Array with requests counters
 var request_per_worker = [];
-// Array with types
-const class_ids = ['R','G','B'];
-
+const answer_interval = 2000;
+var busy_workers = [];
 
 var verbose = false;
 if(args[args.length - 1] == "-v"){
     verbose = true;
     args.pop();
+}
+
+// Create class array from args
+var class_ids = [];
+for(var i = 0; i < args.length; i++){
+    class_ids[i] = args[i];
 }
 
 for(var i in class_ids){
@@ -44,19 +49,34 @@ frontend.on("message", function(){ // Socket add ID client automatically
         console.log("[FRONTEND LOG] New petition from a client");
         console.log("[FRONTEND LOG] Arguments: " + aux.convertArrayToString(arguments));
     }
-
     var args = Array.apply(null, arguments);
-    var class_id = args.pop(); // Get last arg -> type
-	if(workers[class_id].length > 0){ // There is any available worker
+    send_request(args);
+});
+
+
+backend.on("message", function(){
+    if(verbose)
+        console.log("[BACKEND LOG] Arguments: " + aux.convertArrayToString(arguments));
+    var args = Array.apply(null, arguments);
+    var class_id = args.pop(); // Get last arg -> Type
+    if(args.length == 3) { // Msg from worker  Ex. ["Worker1","","Ready"] or ["Client1","","Done"]
         if(verbose)
-            console.log("[FRONTEND LOG] Worker available, forwarding client request");
-        var myWorker = workers[class_id].shift(); // Get first worker (The oldest)
-		var m = [myWorker,''].concat(args); // Ex. ["Worker1","","Client1","","Work"]
-		backend.send(m); // Sending msg to worker
-	}else // No available worker exist
+            console.log("[BACKEND LOG] Message recived from worker");
+        request_per_worker[args[0]] = 0;
+        var worker_id = args[0];
+        if(!processPendingClient(worker_id, class_id)) // Check if there are clients waiting
+            workers[class_id].push(args[0]);
+    }else{ // Response msg for the client through the frontend
         if(verbose)
-            console.log("[FRONTEND LOG] No worker available, saving client petition");
-	    clients[class_id].push({id: args[0],msg: args.slice(2)}); // Save client petition
+            console.log("[BACKEND LOG] Trasmitting the msg to the client");
+        request_per_worker[args[0]]++;
+        var worker_id = args[0];
+        clearTimeout(busy_workers[worker_id].timeout);
+        args = args.slice(2); // Delete worker id and delimitter
+        frontend.send(args);
+        if(!processPendingClient(worker_id, class_id)) // Check if there are clients waiting
+            workers[class_id].push(worker_id);
+    }
 });
 
 // This function is called for two reasons (1.New worker online 2. Worker end your job)
@@ -66,7 +86,7 @@ function processPendingClient(worker_id, class_id){
 		// Get first client data
 		var client = clients[class_id].shift();
 		var msg = [worker_id,'',client.id,''].concat(client.msg);
-        backend.send(msg);
+        send_msg_worker(msg);
         if(verbose) {
             console.log("[BACKEND LOG] Sending client (%s) request to worker (%s) through backend.", msg[2], msg[0]);
             console.log("[BACKEND LOL] Message: " + aux.convertArrayToString(msg));
@@ -88,31 +108,40 @@ function showStatistics(){
     }
 }
 
+function send_msg_worker(msg){
+    var my_worker = msg[0];
+    backend.send(msg);
+    busy_workers[my_worker] = {};
+    busy_workers[my_worker].msg = msg.slice(2);
+    busy_workers[my_worker].timeout = setTimeout(generateTimeoutHandler(my_worker), answer_interval);
+}
+
+function send_request(args){
+    var class_id = args.pop(); // Get last arg -> type
+    if(workers[class_id].length > 0){
+        if(verbose)
+            console.log("[FRONTEND LOG] Worker available, forwarding client request");
+
+        var my_worker = workers[class_id].shift();
+        var msg = [my_worker,''].concat(args);
+        send_msg_worker(msg);
+    }else{
+        if(verbose)
+            console.log("[FRONTEND LOG] No worker available, saving client petition");
+        clients[class_id].push({id: args[0],msg: args.slice(2)});
+    }
+}
+
+function generateTimeoutHandler(worker_id){
+    return function(){
+        var msg = busy_workers[worker_id].msg;
+        delete busy_workers[worker_id];
+        send_request(msg);
+    }
+}
+
 // Only in mode verbose
 process.on('SIGINT', showStatistics);
 
-backend.on("message", function(){
-    if(verbose)
-        console.log("[BACKEND LOG] Arguments: " + aux.convertArrayToString(arguments));
-	var args = Array.apply(null, arguments);
-	var class_id = args.pop(); // Get last arg -> Type
-	if(args.length == 3) { // Msg from worker  Ex. ["Worker1","","Ready"] or ["Client1","","Done"]
-        if(verbose)
-            console.log("[BACKEND LOG] Message recived from worker");
-        request_per_worker[args[0]] = 0;
-        var worker_id = args[0];
-        if(!processPendingClient(worker_id, class_id)) // Check if there are clients waiting
-            workers[class_id].push(args[0]);
-	}else{ // Response msg for the client through the frontend
-        if(verbose)
-	        console.log("[BACKEND LOG] Trasmitting the msg to the client");
-        request_per_worker[args[0]]++;
-        var worker_id = args[0];
-        args = args.slice(2); // Delete worker id and delimitter
-        frontend.send(args);
-        if(!processPendingClient(worker_id, class_id)) // Check if there are clients waiting
-            workers[class_id].push(worker_id);
-	}
-});
 
 
