7528cb99e61f010a70ae25712935be8604096653
Vicent Pérez <vicennt@gmail.com>
2017-12-09 17:04:53 +0100

    Generate stadistics & identify type of work

--- myworker.js
+++ myworker.js
@@ -9,6 +9,7 @@ var backend_url = args[0] || "tcp://localhost:8060";
 var my_id = args[1] || "NONE";
 var disp_text = args[2] || "Ready";
 var reply_text = args[3] || "Done";
+var class_id = args[4] || "B";
 var verbose = false;
 
 if(args[args.length - 1] == "-v"){
@@ -20,10 +21,10 @@ if(my_id != "NONE")
     req.identity = my_id;
 
 req.connect(backend_url);
-req.send(disp_text);
+req.send([disp_text, class_id]);
 
 if(verbose){
-    console.log('Worker (%s) connected to "%s"', my_id, backend_url);
+    console.log('Worker (%s) class (%s) connected to "%s"', my_id, class_id, backend_url);
     console.log('Worker (%s) has sent its first connection message: "%s"', my_id, disp_text);
 }
 
@@ -32,7 +33,7 @@ req.on("message", function(client, delimiter, msg){
         console.log('Worker (%s) has received request ["%s"] from client (%s)', my_id, msg.toString(), client);
 
 	setTimeout(function(){
-		req.send([client,'',reply_text]);
+		req.send([client,'',reply_text, class_id]);
 		if(verbose)
 		    console.log('Worker (%s) has sent its reply "%s"', my_id, reply_text);
 	}, 1000);
